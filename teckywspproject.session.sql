c7e1aff813f0094e49fa9d3e0370a64ac444bed4
HarryTai <harry_tai@msn.com>
2022-07-05 12:04:13 +0800

    game display test1

--- teckywspproject.session.sql
+++ teckywspproject.session.sql
@@ -1,6 +1,6 @@
 -- psql 
 -- you can use it in vscode sqltools or copy to terminal/powershell
-CREATE DATABASE teckywspproject;
+-- CREATE DATABASE teckywspproject;
 --
 CREATE TABLE player (
     user_id SERIAL primary key,
@@ -20,27 +20,27 @@ CREATE TABLE game_plants_list (
 -- SET DEFAULT 0,
 --     ALTER COLUMN items_count
 -- SET NOT NULL;
-INSERT into game_plants_list (items_name, items_count)
+INSERT into game_plants_list (items_name)
 VALUES ('carrot');
-INSERT into game_plants_list (items_name, items_count)
+INSERT into game_plants_list (items_name)
 VALUES ('corn');
-INSERT into game_plants_list (items_name, items_count)
+INSERT into game_plants_list (items_name)
 VALUES ('yellow_flower');
-INSERT into game_plants_list (items_name, items_count)
+INSERT into game_plants_list (items_name)
 VALUES ('red_flower');
-INSERT into game_plants_list (items_name, items_count)
+INSERT into game_plants_list (items_name)
 VALUES ('blue_flower');
-INSERT into game_plants_list (items_name, items_count)
+INSERT into game_plants_list (items_name) 
 VALUES ('pumpkin');
-INSERT into game_plants_list (items_name, items_count)
+INSERT into game_plants_list (items_name)
 VALUES ('lettuce');
-INSERT into game_plants_list (items_name, items_count)
+INSERT into game_plants_list (items_name)
 VALUES ('green_trees');
-INSERT into game_plants_list (items_name, items_count)
+INSERT into game_plants_list (items_name)
 VALUES ('brown_trees');
-INSERT into game_plants_list (items_name, items_count)
+INSERT into game_plants_list (items_name)
 VALUES ('small_house');
-INSERT into game_plants_list (items_name, items_count)
+INSERT into game_plants_list (items_name)
 VALUES ('big_house');
 --
 select *
