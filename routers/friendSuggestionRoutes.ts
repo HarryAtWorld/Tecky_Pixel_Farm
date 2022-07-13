c7dad48fe2eaf1c1c58e74b5f124444911626ba2
Alex Ng <alex.ng19941008@gmail.com>
2022-07-13 16:45:52 +0800

    fixed fdSG big error

--- routers/friendSuggestionRoutes.ts
+++ routers/friendSuggestionRoutes.ts
@@ -7,26 +7,27 @@ export const friendSuggestion = express.Router();
 friendSuggestion.get("/", getFDSuggestion);
 
 async function getFDSuggestion(req: Request, res: Response) {
-  const user = req.session["user"]
-  const data_suggestion = await client.query(`SELECT 
+  const user = req.session["user"];
+  const data_suggestion = await client.query(
+    `SELECT 
     id, user_name 
     FROM user_info 
     WHERE user_info.id != $1 
-    AND user_info.id != (
+    AND user_info.id NOT IN(
         select 
         user_id_b 
         from relationship 
         where user_id_a = $1
         ) AND 
-        user_info.id != (
+        user_info.id NOT IN (
             select user_id_a 
             from relationship 
             where user_id_b = $1
             )
     ORDER BY random()
-    limit 5;`,[
-    user.id
-    ]);
+    limit 5;`,
+    [user.id]
+  );
   console.log(`This is data of fd suggestion`);
   console.log(data_suggestion.rows);
 
